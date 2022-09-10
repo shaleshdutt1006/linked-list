1f48c472f9d4ba12f45c02683deec087d1d12f36
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-10 21:40:57 +0530

    Refactored : Searching the elements of linked list

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -27,32 +27,15 @@ public class LinkedList<T> {
         currnode.next = newNode;
     }
 
-    public void delete() {
-        if (head == null) {
-            System.out.println("The list is empty");
-        }
-        if (head.next == null) {
-            head = null;
-        }
-        Node secondLast = head;
-        Node lastNode = head.next;
-        while (lastNode.next != null) {
-            lastNode = lastNode.next;
-            secondLast = secondLast.next;
-        }
-        secondLast.next = null;
-
-    }
-
-    public void display() {
-        if (head == null) {
-            System.out.println("List is empty");
-        }
+    public boolean search(T value) {
         Node currnode = head;
         while (currnode != null) {
-            System.out.println(currnode.data + " ");
+            if (currnode.data.equals(value)) {
+                return true;
+            }
             currnode = currnode.next;
         }
+        return false;
     }
 
     public static void main(String[] args) {
@@ -60,9 +43,7 @@ public class LinkedList<T> {
         list.add(56);
         list.add(30);
         list.add(70);
-        list.add("dutt");
         list.add(2.6f);
-        list.delete();
-        list.display();
+        System.out.println(list.search(30));
     }
 }
