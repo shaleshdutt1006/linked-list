da8c224b1a23b8533cda0c7a49304ecbdb5be9f2
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-10 22:23:47 +0530

    Refactored : Searching the Elements And Insertion to the location of Element

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -1,12 +1,12 @@
 package com.bridgelabz;
 
 public class LinkedList<T> {
-    public Node head;
-    public Node tail;
+    public Node<T> head;
+    public Node<T> tail;
 
-    class Node {
+    class Node<T> {
         T data;
-        Node next;
+        Node<T> next;
 
         Node(T data) {
             this.data = data;
@@ -15,27 +15,56 @@ public class LinkedList<T> {
     }
 
     public void add(T data) {
-        Node newNode = new Node(data);
+        Node<T> newNode = new Node<T>(data);
         if (head == null) {
             head = newNode;
             return;
         }
-        Node currnode = head;
+        Node<T> currnode = head;
         while (currnode.next != null) {
             currnode = currnode.next;
         }
         currnode.next = newNode;
     }
 
-    public boolean search(T value) {
-        Node currnode = head;
+    public int search(T value) {
+        int index = 0;
+        Node<T> currnode = head;
         while (currnode != null) {
             if (currnode.data.equals(value)) {
-                return true;
+                return index;
             }
+            index++;
             currnode = currnode.next;
         }
-        return false;
+        return -1;
+    }
+
+    public void insertAt(int pos, T value) {
+        Node<T> node = new Node<T>(value);
+        if (pos == 0) {
+            node.next = head;
+            head = node;
+            return;
+        }
+        Node<T> previous = head;
+        for (int i = 0; i < pos; i++) {
+            previous = previous.next;
+        }
+        node.next = previous.next;
+        previous.next = node;
+    }
+
+    public void display() {
+        if (head == null) {
+            System.out.println("List is empty");
+        }
+        Node currnode = head;
+        while (currnode != null) {
+            System.out.println(currnode.data + " ");
+            currnode = currnode.next;
+        }
+
     }
 
     public static void main(String[] args) {
@@ -43,7 +72,9 @@ public class LinkedList<T> {
         list.add(56);
         list.add(30);
         list.add(70);
-        list.add(2.6f);
-        System.out.println(list.search(30));
+        System.out.println("Position of the element is : " + list.search(30));
+        list.insertAt(list.search(30), 40);
+        System.out.println("Linked list after insertion is : ");
+        list.display();
     }
 }
