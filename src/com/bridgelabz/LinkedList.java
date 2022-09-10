18502af4b48dfe01340291aed4c5edf2b63773fd
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-10 17:19:35 +0530

    Refactored : Appending Elements in a Linked List Program

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -7,41 +7,44 @@ public class LinkedList {
     static class Node {
         int data;
         Node next;
+
         Node(int data) {    // when we call this constructor it will create new node everytime
             this.data = data;
             this.next = null;
         }
     }
-
-    public void add(int data) {
+    public void append(int data) {
         Node newNode = new Node(data);
 
         if (head == null) {
             head = newNode;
-            tail = newNode;
-        } else {
-            newNode.next = head;
-            head = newNode;
+            return;
         }
+        Node currnode = head;
+        while (currnode.next != null) {
+            currnode = currnode.next;
+        }
+        currnode.next = newNode;
     }
-
     public void display() {
-        Node current = head;
+
         if (head == null) {
             System.out.println("List is empty");
         }
-        while (current != null) {
-            System.out.println(current.data + " ");
-            current = current.next;
+        Node currnode = head;
+        while (currnode != null) {
+            System.out.println(currnode.data + " ");
+            currnode = currnode.next;
         }
+        System.out.println("null");
     }
 
     public static void main(String[] args) {
 
         LinkedList list = new LinkedList();
-        list.add(70);
-        list.add(30);
-        list.add(56);
+        list.append(56);
+        list.append(30);
+        list.append(70);
         list.display();
     }
 }
