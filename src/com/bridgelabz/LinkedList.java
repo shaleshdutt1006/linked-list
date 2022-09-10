538a69fe87844b1f2ea8f48a9beb38064d4e3bcb
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-11 00:51:25 +0530

    Refactored : Sorting the Elements in Ascending Order

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -1,78 +1,63 @@
 package com.bridgelabz;
 
 public class LinkedList<T> {
-    public Node<T> head;
-    public Node<T> tail;
+    public Node head;
 
-    class Node<T> {
-        T data;
-        Node<T> next;
+    class Node {
+        int data;
+        Node next;
 
-        Node(T data) {
+        Node(int data) {
             this.data = data;
             this.next = null;
         }
     }
 
-    public void add(T data) {
-        Node<T> newNode = new Node<T>(data);
+    public void add(int data) {
+        Node newNode = new Node(data);
         if (head == null) {
             head = newNode;
             return;
         }
-        Node<T> currnode = head;
+        Node currnode = head;
         while (currnode.next != null) {
             currnode = currnode.next;
         }
         currnode.next = newNode;
     }
 
-    public int search(T value) {
-        int index = 0;
-        Node<T> currnode = head;
-        while (currnode != null) {
-            if (currnode.data.equals(value)) {
-                return index;
-            }
-            index++;
-            currnode = currnode.next;
-        }
-        return -1;
-    }
-
-    public void delete(int pos, T value) {
-        if (pos == 0) {
-            head = head.next;
-            return;
-        }
-        Node previous = head;
-        for (int i = 0; i < pos - 1; i++) {
-            previous = previous.next;
-        }
-        previous.next = previous.next.next;
-    }
-
-    public void display() {
-        int count = 0;
-        if (head == null) {
-            System.out.println("List is empty");
-        }
-        Node currnode = head;
-        while (currnode != null) {
-            System.out.println(currnode.data + " ");
-            currnode = currnode.next;
-            count++;
-        }
-    }
-
     public int size() {
         int count = 0;
-        Node<T> currnode = head;
+        Node currnode = head;
         while (currnode != null) {
             currnode = currnode.next;
             count++;
         }
         return count;
+    }
+
+    public void sorting() {
+        for (int i = 0; i < size(); i++) {
+            Node currnode = head;
+            Node nextnode = currnode.next;
+            int temp;
+            for (int j = 0; j < size() - 1; j++) {
+
+                if (currnode.data > nextnode.data) {
+                    temp = currnode.data;
+                    currnode.data = nextnode.data;
+                    nextnode.data = temp;
+                }
+                currnode = nextnode;
+                nextnode = nextnode.next;
+            }
+        }
+        System.out.println("Sorted array is this : ");
+        Node node = head;
+        while (node != null) {
+            System.out.println(node.data + " ");
+            node = node.next;
+        }
     }
 
     public static void main(String[] args) {
@@ -81,10 +66,6 @@ public class LinkedList<T> {
         list.add(30);
         list.add(40);
         list.add(70);
-        System.out.println("Position of the element " + "40" + " is : " + list.search(40));
-        list.delete(list.search(40), 40);
-        System.out.println("Linked list after deletion is : ");
-        list.display();
-        System.out.println("Size of linked list is : " + list.size());
+        list.sorting();
     }
 }
